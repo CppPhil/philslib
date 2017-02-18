4255d17de9e11cc8ac35c2fbd4c6ac18fa0fc30b
Phil <CppPhil@users.noreply.github.com>
2017-02-18 09:13:17 +0100

    update

---
+++ include/memory.hpp
@@ -0,0 +1,8 @@
+/*! \file memory.hpp
+ *  \brief This header defines various memory management utilities.
+**/
+
+#ifndef INCG_PL_MEMORY_HPP
+#define INCG_PL_MEMORY_HPP
+
+#endif // INCG_PL_MEMORY_HPP
---
+++ src/memory.cpp
@@ -0,0 +1,3 @@
+#include "include/memory.hpp"
+
+
