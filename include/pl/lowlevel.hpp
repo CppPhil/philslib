85decd5ca27b7e2be1a584c04c711d099b5a71dc
Phil <CppPhil@users.noreply.github.com>
2017-07-09 12:01:09 +0200

    extended documentation on pl::unrelated_pointer_cast

--- include/pl/lowlevel.hpp
+++ include/pl/lowlevel.hpp
@@ -21,6 +21,8 @@ using Byte = unsigned char;
 
 /*!
  * \brief Casts a raw pointer type to another unrelated pointer type.
+ *        This partially replaces reinterpret_cast, only use reinterpret_cast
+ *        for pointer to integer (and vice versa) conversions.
  * \param p The pointer to cast to CastTo.
  * \return The result of casting p to type CastTo
  * \note Note that all raw pointers without low level constness
@@ -29,6 +31,22 @@ using Byte = unsigned char;
  * \note The type to cast to called CastTo must be a raw pointer type.
  * \note This is the overload for pointers without low level constness.
  * \note You must supply the template type parameter CastTo.
+ * \warning Like with reinterpret_cast it is easy to violate the strict aliasing rules
+ *          with this function. Two pointers may never refer to the same object
+ *          if they have different types, unless at least one of the pointers
+ *          is cv void *, cv char *, cv unsigned char *, cv std::byte * (since C++17),
+ *          or there there is a based/derived relation between the pointed to types
+ *          or there merely is an unsigned/signed mismatch between the pointed to types.
+ *          This also makes common code like
+ *          // assuming unsigned long long and double have the same width in bytes
+ *          double d;
+ *          unsigned long long ull = 0;
+ *          d = *reinterpret_cast<double *>(&ull);
+ *          undefined behavior.
+ *          Note that in many cases you can use std::memcpy, defined in <cstring>
+ *          instead.
+ *          Basically any usage of reinterpret_cast and pl::unrelated_pointer_cast
+ *          should be suspect.
 **/
 template <typename CastTo>
 CastTo unrelated_pointer_cast(PL_IN_OPT void *p)
@@ -41,6 +59,8 @@ CastTo unrelated_pointer_cast(PL_IN_OPT void *p)
 
 /*!
  * \brief Casts a raw pointer type to another unrelated raw pointer type.
+ *        This partially replaces reinterpret_cast, only use reinterpret_cast
+ *        for pointer to integer (and vice versa) conversions.
  * \param p The raw pointer to be casted to type CastTo.
  * \return The result of casting p to type CastTo.
  * \note Note that all raw pointers with low level constness
@@ -50,6 +70,22 @@ CastTo unrelated_pointer_cast(PL_IN_OPT void *p)
  * \note CastTo must have low level constness
  * \note This is the overload for raw pointers with low level constness.
  * \note You must supply the template type parameter CastTo.
+ * \warning Like with reinterpret_cast it is easy to violate the strict aliasing rules
+ *          with this function. Two pointers may never refer to the same object
+ *          if they have different types, unless at least one of the pointers
+ *          is cv void *, cv char *, cv unsigned char *, cv std::byte * (since C++17),
+ *          or there there is a based/derived relation between the pointed to types
+ *          or there merely is an unsigned/signed mismatch between the pointed to types.
+ *          This also makes common code like
+ *          // assuming unsigned long long and double have the same width in bytes
+ *          double d;
+ *          unsigned long long ull = 0;
+ *          d = *reinterpret_cast<double *>(&ull);
+ *          undefined behavior.
+ *          Note that in many cases you can use std::memcpy, defined in <cstring>
+ *          instead.
+ *          Basically any usage of reinterpret_cast and pl::unrelated_pointer_cast
+ *          should be suspect.
 **/
 template <typename CastTo>
 CastTo unrelated_pointer_cast(PL_IN_OPT const void *p)
