8fe3f7528221d04a8d022523cc08ea595beb93bc
Phil <CppPhil@users.noreply.github.com>
2017-07-09 13:06:55 +0200

    added version checking macros

--- include/pl/compiler.hpp
+++ include/pl/compiler.hpp
@@ -6,12 +6,6 @@
 #define INCG_PL_COMPILER_HPP
 
 /*!
- * \todo Add macros to get the version number of the compiler being used.
- *       Also offer a convenient interface to compare the version number
- *       of the compiler being used to a given version number.
-**/
-
-/*!
  * \def PL_COMPILER_CLANG
  * \brief PL_COMPILER will be defined as this if clang is being used.
 **/
@@ -46,6 +40,34 @@
  *        depending on which compiler is being used.
 **/
 
+/*!
+ * \def PL_COMPILER_MAJOR
+ * \brief The major version of the compiler.
+**/
+
+/*!
+ * \def PL_COMPILER_MINOR
+ * \brief The minor version of the compiler.
+**/
+
+/*!
+ * \def PL_COMPILER_PATCH
+ * \brief The patch level of the compiler.
+**/
+
+/*!
+ * \def PL_COMPILER_VERSION_CHECK(major, minor, patch)
+ * \brief Can be used to create a compiler version using a major, minor
+ *        and patch number. Useful to compare the current compiler version
+ *        to a specific version.
+**/
+
+/*!
+ * \def PL_COMPILER_VERSION
+ * \brief The current compiler version, may be compared to a number generated
+ *        with PL_COMPILER_VERSION_CHECK
+**/
+
 #define PL_COMPILER_CLANG (0x0004) /* meaningless number */
 #define PL_COMPILER_ICC (0x0005) /* meaningless number */
 #define PL_COMPILER_MSVC (0x0006) /* meaningless number */
@@ -64,4 +86,33 @@
 #   define PL_COMPILER PL_COMPILER_UNKNOWN
 #   warning "Compiler could not be detected"
 #endif
+
+#if PL_COMPILER == PL_COMPILER_CLANG
+    #define PL_COMPILER_MAJOR __clang_major__
+    #define PL_COMPILER_MINOR __clang_minor__
+    #define PL_COMPILER_PATCH __clang_patchlevel__
+#elif PL_COMPILER == PL_COMPILER_ICC
+    #define PL_COMPILER_MAJOR __INTEL_COMPILER / 100
+    #define PL_COMPILER_MINOR __INTEL_COMPILER % 100 / 10
+    #define PL_COMPILER_PATCH __INTEL_COMPILER % 10
+#elif PL_COMPILER == PL_COMPILER_MSVC
+    #define PL_COMPILER_MAJOR _MSC_FULL_VER / 10000000
+    #define PL_COMPILER_MINOR _MSC_FULL_VER % 10000000 / 100000
+    #define PL_COMPILER_PATCH _MSC_FULL_VER % 100000
+#elif PL_COMPILER == PL_COMPILER_GCC
+    #define PL_COMPILER_MAJOR __GNUC__
+    #define PL_COMPILER_MINOR __GNUC_MINOR__
+    #define PL_COMPILER_PATCH __GNUC_PATCHLEVEL__
+#else
+    #define PL_COMPILER_MAJOR (0x00)
+    #define PL_COMPILER_MINOR (0x00)
+    #define PL_COMPILER_PATCH_VERSION (0x00)
+#endif
+
+#define PL_COMPILER_VERSION_CHECK(major, minor, patch) \
+    (((major) * 16777216) + ((minor) * 65536) + (patch))
+
+#define PL_COMPILER_VERSION \
+    PL_COMPILER_VERSION_CHECK(PL_COMPILER_MAJOR, PL_COMPILER_MINOR, PL_COMPILER_PATCH)
+
 #endif // INCG_PL_COMPILER_HPP
