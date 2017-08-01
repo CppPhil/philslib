0a6d41df6ec51207d1e05111439a60691ed8072e
Phil <CppPhil@users.noreply.github.com>
2017-08-01 19:39:59 +0200

    added make_aray

--- include/pl/cont/make_array.hpp
+++ include/pl/cont/make_array.hpp
@@ -5,31 +5,55 @@
 **/
 #ifndef INCG_PL_CONT_MAKE_ARRAY_HPP
 #define INCG_PL_CONT_MAKE_ARRAY_HPP
+#include "../meta/conjunction.hpp" // pl::meta::conjunction
+#include "../meta/is_reference_wrapper.hpp" // pl::meta::is_not_reference_wrapper
 #include "../annotations.hpp" // PL_IN
 #include <utility> // std::forward
 #include <array> // std::array
-#include <type_traits> // std::common_type_t
+#include <type_traits> // std::common_type
 
 namespace pl
 {
 namespace cont
 {
+namespace detail
+{
+/*!
+ * \brief Implementation of makeArray. Do not use directly.
+**/
+template <typename DesiredType, typename ...>
+struct ReturnTypeHelper
+{
+    using type = DesiredType;
+};
+
+/*!
+ * \brief Implementation of makeArray. Do not use directly.
+**/
+template <typename ...Types>
+struct ReturnTypeHelper<void, Types ...>
+    : public std::common_type<Types ...>
+{
+    static_assert(::pl::meta::conjunction<::pl::meta::is_not_reference_wrapper<Types> ...>::value,
+                  "Types cannot contain reference_wrappers when DesiredType is void");
+};
+
+/*!
+ * \brief Implementation of makeArray. Do not use directly.
+**/
+template <typename DesiredType, typename ...Types>
+using ReturnType = std::array<typename ReturnTypeHelper<DesiredType, Types ...>::type, sizeof...(Types)>;
+} // namespace detail
+
 /*!
  * \brief Creates a std::array from a template type parameter pack.
  * \param args The arguments to construct the std::array returned from.
  * \return The resulting std::array.
- * \note The resulting std::array will use the common type for all of its
- *       elements. Some of the values passed in may be converted to that common type.
 **/
-template <typename ...Args>
-constexpr auto makeArray(PL_IN Args &&...args)
-    -> std::array<std::common_type_t<Args ...>, sizeof...(args)>
+template <typename DesiredType = void, typename ...Args>
+constexpr detail::ReturnType<DesiredType, Args ...> makeArray(Args &&...args)
 {
-    return {
-        static_cast<std::common_type_t<Args ...>>(
-            std::forward<Args>(args)
-        )...
-    };
+    return { std::forward<Args>(args)... };
 }
 } // namespace cont
 } // namespace pl
--- include/pl/meta/is_reference_wrapper.hpp
+++ include/pl/meta/is_reference_wrapper.hpp
@@ -1,12 +1,14 @@
 /*!
  * \file is_reference_wrapper.hpp
- * \brief Exports the is_reference_wrapper meta function to checke whether
+ * \brief Exports the is_reference_wrapper meta function to check whether
  *        or not a type is a std::reference_wrapper type.
+ *        Also exports is_not_reference_wrapper.
 **/
 #ifndef INCG_PL_META_IS_REFERENCE_WRAPPER_HPP
 #define INCG_PL_META_IS_REFERENCE_WRAPPER_HPP
+#include "negation.hpp" // pl::meta::negation
 #include <functional> // std::reference_wrapper
-#include <type_traits> // std::false_type, std::true_type
+#include <type_traits> // std::false_type, std::true_type, std::decay_t
 
 namespace pl
 {
@@ -37,6 +39,12 @@ struct is_reference_wrapper<std::reference_wrapper<Type>>
     : public std::true_type
 {
 };
+
+/*!
+ * \brief Negation of is_reference_wrapper.
+**/
+template <typename Type>
+using is_not_reference_wrapper = negation<is_reference_wrapper<std::decay_t<Type>>>;
 } // namespace meta
 } // namespace pl
 #endif // INCG_PL_META_IS_REFERENCE_WRAPPER_HPP
