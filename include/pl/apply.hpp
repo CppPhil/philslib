ae63055a499bd8c55c8e0784a8affcf327f79ea2
Phil <CppPhil@users.noreply.github.com>
2017-08-06 13:32:49 +0200

    Implemented apply

---
+++ include/pl/apply.hpp
@@ -0,0 +1,52 @@
+/*!
+ * \file apply.hpp
+ * \brief Exports the apply function template.
+**/
+#ifndef INCG_PL_APPLY_HPP
+#define INCG_PL_APPLY_HPP
+#include "annotations.hpp" // PL_IN
+#include "invoke.hpp" // pl::invoke
+#include <cstddef> // std::size_t
+#include <utility> // std::index_sequence, std::make_index_sequence, std::forward
+#include <tuple> // std::get, std::tuple_size
+#include <type_traits> // std::decay_t
+
+namespace pl
+{
+namespace detail
+{
+/*!
+ * \brief Implementation function of apply; not to be used directly!
+**/
+template <typename Callable, typename TupleLike, std::size_t ...Indices>
+constexpr auto applyImpl(PL_IN Callable &&callable, PL_IN TupleLike &&tupleLike,
+                         std::index_sequence<Indices ...>) -> decltype(auto)
+{
+    return invoke(std::forward<Callable>(callable),
+                  std::get<Indices>(std::forward<TupleLike>(tupleLike)) ...);
+}
+} // namespace detail
+
+/*!
+ * \brief Invokes the callable object 'callable' with a tuple of arguments.
+ * \param callable The callable object to be invoked.
+ * \param tupleLike The tuple whose elements to be used as arguments to 'callable'
+ * \return The result that was returned by 'callable'.
+ * \note The 'tupleLike' need not be std::tuple, and instead may be anything
+ *       that supports std::get and std::tuple_size; in particular,
+ *       std::array and std::pair may be used.
+**/
+template <typename Callable, typename TupleLike>
+constexpr auto apply(PL_IN Callable &&callable,
+                     PL_IN TupleLike &&tupleLike) -> decltype(auto)
+{
+    return detail::applyImpl(std::forward<Callable>(callable),
+                             std::forward<TupleLike>(tupleLike),
+                             std::make_index_sequence<
+                                std::tuple_size<
+                                    std::decay_t<TupleLike>
+                                >::value
+                             >{ });
+}
+} // namespace pl
+#endif // INCG_PL_APPLY_HPP
--- include/pl/invoke.hpp
+++ include/pl/invoke.hpp
@@ -4,6 +4,7 @@
 **/
 #ifndef INCG_PL_INVOKE_HPP
 #define INCG_PL_INVOKE_HPP
+#include "annotations.hpp" // PL_IN
 #include <ciso646> // not
 #include <type_traits> // std::enable_if_t, std::is_member_pointer, std::decay_t, std::result_of_t
 #include <functional> // std::mem_fn
@@ -15,6 +16,7 @@ namespace pl
  * \brief Calls 'callable' with 'args'.
  * \param callable The callable object to be invoked.
  * \param args The arguments to pass to 'callable'.
+ * \return The result of invoking 'callable' with 'args'.
  *
  * Calls the callable 'callable' with 'args'.
  * Offers a uniform interface to invoke free functions,
@@ -22,7 +24,7 @@ namespace pl
  * as well as access non-static data members through member object pointers.
 **/
 template <typename Callable, typename ...Args>
-auto invoke(Callable &&callable, Args &&...args)
+auto invoke(PL_IN Callable &&callable, PL_IN Args &&...args)
     noexcept(noexcept(std::mem_fn(callable)(std::forward<Args>(args) ...)))
     -> std::enable_if_t<
            std::is_member_pointer<std::decay_t<Callable>>::value,
@@ -35,6 +37,7 @@ auto invoke(Callable &&callable, Args &&...args)
 * \brief Calls 'callable' with 'args'.
 * \param callable The callable object to be invoked.
 * \param args The arguments to pass to 'callable'.
+* \return The result of invoking 'callable' with 'args'.
 *
 * Calls the callable 'callable' with 'args'.
 * Offers a uniform interface to invoke free functions,
@@ -42,7 +45,7 @@ auto invoke(Callable &&callable, Args &&...args)
 * as well as access non-static data members through member object pointers.
 **/
 template <typename Callable, typename ...Args>
-auto invoke(Callable &&callable, Args &&...args)
+auto invoke(PL_IN Callable &&callable, PL_IN Args &&...args)
     noexcept(noexcept(std::forward<Callable>(callable)(std::forward<Args>(args) ...)))
     -> std::enable_if_t<
            not std::is_member_pointer<std::decay_t<Callable>>::value,
