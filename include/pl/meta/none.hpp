5b55004cb3d3b42173a671e2e75a6dfac9d006e1
Phil <CppPhil@users.noreply.github.com>
2017-08-12 17:41:48 +0200

    Added None meta function

---
+++ include/pl/meta/none.hpp
@@ -0,0 +1,23 @@
+/*!
+ * \file none.hpp
+ * \brief Exports the None meta function.
+**/
+#ifndef INCG_PL_META_NONE_HPP
+#define INCG_PL_META_NONE_HPP
+#include "bool_constant.hpp" // pl::meta::bool_constant
+#include "any.hpp" // pl::meta::any
+#include <ciso646> // not
+
+namespace pl
+{
+namespace meta
+{
+/*!
+ * \brief Will behave like std::true_type if none of the traits passed in
+ *        are satisfied. Otherwise will behave like std::false_type.
+**/
+template <typename ...Traits>
+using None = bool_constant<not Any<Traits ...>::value>;
+} // namespace meta
+} // namespace pl
+#endif // INCG_PL_META_NONE_HPP
