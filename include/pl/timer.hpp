b3bc6e5578f62f4c63259aba5c50473881ebb87d
Phil <CppPhil@users.noreply.github.com>
2017-08-27 17:43:49 +0200

    Timer added.

---
+++ include/pl/timer.hpp
@@ -0,0 +1,46 @@
+/*!
+ * \file timer.hpp
+ * \brief Exports the Timer type.
+**/
+#ifndef INCG_PL_TIMER_HPP
+#define INCG_PL_TIMER_HPP
+#include <chrono> // std::chrono::steady_clock, std::chrono::time_point
+
+namespace pl
+{
+/*!
+ * \brief A timer that can be used to measure durations of time.
+**/
+class Timer
+{
+public:
+    using this_type = Timer;
+
+    /*!
+     * \brief Constructs the Timer object.
+     *        The time stored is initialized with the current time.
+     *        Effectively 'starts' the Timer.
+    **/
+    Timer();
+
+    /*!
+     * \brief Calculates the distance between the current time and the time
+     *        stored in this Timer object.
+     * \return The duration between the current time (when this function is
+     *         invoked) and the time stored. The return value can be cast
+     *         to the desired duration type using std::chrono::duration_cast.
+    **/
+    std::chrono::steady_clock::duration elapsedTime() const;
+
+    /*!
+     * \brief Resets the time stored. The time stored is discarded and
+     *        replaced with the current time (when this function is invoked)
+     * \return A reference to this Timer object.
+    **/
+    this_type &reset();
+
+private:
+    std::chrono::time_point<std::chrono::steady_clock> m_timeStored; /*!< The time stored */
+};
+} // namespace pl
+#endif // INCG_PL_TIMER_HPP
---
+++ src/pl/timer.cpp
@@ -0,0 +1,21 @@
+#include "include/pl/timer.hpp"
+
+namespace pl
+{
+Timer::Timer()
+    : m_timeStored{ std::chrono::steady_clock::now() }
+{
+}
+
+std::chrono::steady_clock::duration Timer::elapsedTime() const
+{
+    return std::chrono::steady_clock::now() - m_timeStored;
+}
+
+Timer &Timer::reset()
+{
+    // replace the time stored with the current time.
+    m_timeStored = std::chrono::steady_clock::now();
+    return *this;
+}
+} // namespace pl
