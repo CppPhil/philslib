4b7e9fef4beb642db9996ac859aa83c82e301592
Phil <CppPhil@users.noreply.github.com>
2017-09-07 17:28:27 +0200

    Remaned unqualified to uncvref

--- include/pl/meta/uncvref.hpp
+++ include/pl/meta/uncvref.hpp
@@ -1,5 +1,9 @@
-#ifndef INCG_PL_META_UNQUALIFIED_HPP
-#define INCG_PL_META_UNQUALIFIED_HPP
+/*!
+ * \file uncvref.hpp
+ * \brief Exports the uncvref meta function.
+**/
+#ifndef INCG_PL_META_UNCVREF_HPP
+#define INCG_PL_META_UNCVREF_HPP
 #include <type_traits> // std::remove_cv, std::remove_reference_t
 
 namespace pl
@@ -7,24 +11,24 @@ namespace pl
 namespace meta
 {
 /*!
- * \brief Meta function to get the unqualifid type of a type.
+ * \brief Meta function to get the unqualified type of a type.
  *
  * The nested type type will have its reference type modifiers removed
  * and will have its const and volatile type qualifiers removed.
 **/
 template <typename Type>
-struct unqualified
+struct uncvref
     : public std::remove_cv<std::remove_reference_t<Type>>
 {
 };
 
 /*!
  * \brief C++14 style meta function helper that is an alias of the nested type
- *        type of pl::unqualified. Reduces the necessity of the typename keyword
+ *        type of pl::uncvref. Reduces the necessity of the typename keyword
  *        in application code.
 **/
 template <typename Type>
-using unqualified_t = typename unqualified<Type>::type;
+using uncvref_t = typename uncvref<Type>::type;
 } // namespace meta
 } // namespace pl
-#endif // INCG_PL_META_UNQUALIFIED_HPP
+#endif // INCG_PL_META_UNCVREF_HPP
