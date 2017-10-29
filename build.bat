f6977ca9d67c9d7b9e0ec9f2883c54a0882818bc
Phil <CppPhil@users.noreply.github.com>
2017-10-29 13:59:07 +0100

    Support for building with x86 and x64

--- build.bat
+++ build.bat
@@ -4,27 +4,60 @@ If "%~1"=="" goto ERROR
 
 If "%~2"=="" goto ERROR
 
+If "%~3"=="" goto ERROR
+
 bitsadmin.exe /transfer "doctest" "https://raw.githubusercontent.com/onqtam/doctest/master/doctest/doctest.h" "%CD%\test\doctest.h"
 
+set isx86=false
+set isx64=false
+
+If "%3"=="x86" (
+    set isx86=true
+) Else (
+    If "%3"=="x64" (
+        set isx64=true
+    )
+)
+
 If "%2"=="Debug" (
-    mkdir Debug
-    cd Debug
+    If %isx86% equ true (
+        mkdir Debug_x86
+        cd Debug_x86
+    ) Else (
+        If %isx64% equ true (
+            mkdir Debug_x64
+            cd Debug_x64
+        ) Else (
+            goto ERROR
+        )
+    )
+    
     cmake -G %1 -DCMAKE_BUILD_TYPE=Debug ..
-    cmake --build . --config Debug
+    msbuild philslib.sln /p:Configuration=Debug;Platform="%3" /maxcpucount
     cd ..
 )
 
 If "%2"=="Release" (
-    mkdir Release
-    cd Release
+    If %isx86% equ true (
+        mkdir Release_x86
+        cd Release_x86
+    ) Else (
+        If %isx64% equ true (
+            mkdir Release_x64
+            cd Release_x64
+        ) Else (
+            goto ERROR
+        )
+    )
+    
     cmake -G %1 -DCMAKE_BUILD_TYPE=Release ..
-    cmake --build . --config Release
+    msbuild philslib.sln /p:Configuration=Release;Platform="%3" /maxcpucount
     cd ..
 )
 
 EXIT /B 0
 
 :ERROR
-    echo "Invalid parameters. You must pass Visual Studio 15 for instance and Debug or Release"
+    echo "Invalid parameters. You must pass Visual Studio 15 or Visual Studio 14 for the first parameter, Debug or Release for the second parameter and x86 or x64 for the third parameter."
     EXIT /B 1
 
--- generate_build_scripts.sh
+++ generate_build_scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters. Enter Debug or Release."
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters. Enter Debug or Release for the first parameter and x86 or x64 for the second parameter."
     exit 1
 fi
 
@@ -9,16 +9,44 @@ if [ ! -f ./test/doctest.h ]; then
     wget -q https://raw.githubusercontent.com/onqtam/doctest/master/doctest/doctest.h -P ./test
 fi
 
+isx86=false
+isx64=false
+
+if [ "$2" == "x86" ]; then
+    isx86=true
+fi
+
+if [ "$2" == "x64" ]; then
+    isx64=true
+fi
+
 if [ "$1" == "Debug" ]; then
-    mkdir Debug
-    cd Debug
-    cmake -DCMAKE_BUILD_TYPE=Debug ..
+    if [ "$isx86" = true ]; then
+        mkdir Debug_x86
+        cd Debug_x86
+    elif [ "$isx64" = true ]; then
+        mkdir Debug_x64
+        cd Debug_x64
+    else
+        echo "Invalid architecture. The second parameter must be x86 or x64"
+        exit 1
+    fi
+
+    cmake -DCMAKE_BUILD_TYPE=Debug -DARCH=$2 ..
 fi
 
 if [ "$1" == "Release" ]; then
-    mkdir Release
-    cd Release
-    cmake -DCMAKE_BUILD_TYPE=Release ..
+    if [ "$isx86" = true ]; then
+        mkdir Release_x86
+        cd Release_x86
+    elif [ "$isx64" = true ]; then
+        mkdir Release_x64
+        cd Release_x64
+    else
+        echo "Invalid architecture. The second parameter must be x86 or x64"
+        exit 1
+    fi
+    cmake -DCMAKE_BUILD_TYPE=Release -DARCH=$2 ..
 fi
 
 exit 0
