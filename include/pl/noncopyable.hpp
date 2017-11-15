055c2fa9ef3b191afc89ab5333e3003aa3a2672e
Phil <CppPhil@users.noreply.github.com>
2017-11-15 20:27:29 +0100

    Added PL_NONCOPYABLE macro

---
+++ include/pl/noncopyable.hpp
@@ -0,0 +1,19 @@
+/*!
+ * \file noncopyable.hpp
+ * \brief Exports a macro to declare a type as noncopyable.
+**/
+#ifndef INCG_PL_NONCOPYABLE_HPP
+#define INCG_PL_NONCOPYABLE_HPP
+
+/*!
+ * \def PL_NONCOPYABLE(typeIdentifier)
+ * \brief Macro to be placed in the public section of a type.
+ *        Pass the identifier of the type into this macro.
+ *        Used to declare a type as noncopyable.
+**/
+
+#define PL_NONCOPYABLE(typeIdentifier) \
+    typeIdentifier(const typeIdentifier &) = delete; \
+    typeIdentifier &operator=(const typeIdentifier &) = delete
+
+#endif // INCG_PL_NONCOPYABLE_HPP
