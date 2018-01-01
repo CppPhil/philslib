4276361767b7979a13828afe84b69a9d7e75ae73
Phil <CppPhil@users.noreply.github.com>
2018-01-01 03:34:25 +0100

    Added non-member at function

---
+++ include/pl/cont/at.hpp
@@ -0,0 +1,106 @@
+/* This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
+/*!
+ * \file at.hpp
+ * \brief Exports a bounds checked index based interface for containers,
+ *        C-Arrays and std::initializer_lists.
+**/
+#ifndef INCG_PL_CONT_AT_HPP
+#define INCG_PL_CONT_AT_HPP
+#include "../annotations.hpp" // PL_IN
+#include <ciso646> // not
+#include <cstddef> // std::size_t
+#include <stdexcept> // std::out_of_range
+#include <initializer_list> // std::initializer_list
+
+namespace pl
+{
+namespace cont
+{
+/*!
+ * \brief Retrieves a reference to the element at position 'index' in the
+ *        C-Array 'arr'.
+ * \param arr The C-Array to access.
+ * \param index The index of the element in 'arr' to get a reference to.
+ * \return A reference to the element at position 'index' in 'arr'.
+ * \throws std::out_of_range if 'index' is out of bounds.
+**/
+template <typename Ty, std::size_t Size>
+inline constexpr Ty &at(PL_IN Ty (&arr)[Size], std::size_t index)
+{
+    if (not (index < Size)) {
+        throw std::out_of_range{
+            "index was out of bounds in pl::cont::at (C-Array)"
+        };
+    }
+
+    return arr[index];
+}
+
+/*!
+ * \brief Retrieves a reference to the element at position 'index' in the
+ *        container 'container'.
+ * \param container The container to access.
+ * \param index The index of the element in 'container' to get a reference to.
+ * \return A reference to the element at position 'index' in 'container'.
+ * \throws std::out_of_range if 'index' is out of bounds.
+**/
+template <typename Container>
+inline constexpr auto at(PL_IN Container &container, std::size_t index)
+    -> decltype(container[container.size()])
+{
+    if (not (index < container.size())) {
+        throw std::out_of_range{
+            "index was out of bounds in pl::cont::at (container)"
+        };
+    }
+
+    return container[index];
+}
+
+/*!
+ * \brief Retrieves a copy of the element at position 'index' in the
+ *        std::initializer_list 'il'.
+ * \param il The initializer_list to access.
+ * \param index The index of the element to get a copy of.
+ * \return A copy of the element at position 'index'.
+ * \throws std::out_of_range if 'index' is out of bounds.
+**/
+template <typename Ty>
+inline constexpr Ty at(std::initializer_list<Ty> il, std::size_t index)
+{
+    if (not (index < il.size())) {
+        throw std::out_of_range{
+            "index was out of bounds in pl::cont::at (initializer_list)"
+        };
+    }
+
+    return *(il.begin() + index);
+}
+} // namespace cont
+} // namespace pl
+#endif // INCG_PL_CONT_AT_HPP
---
+++ test/src/cont/at_test.cpp
@@ -0,0 +1,141 @@
+/* This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
+#include "../../../include/pl/compiler.hpp"
+#if PL_COMPILER == PL_COMPILER_GCC
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wmissing-noreturn"
+#endif // CR_COMPILER == CR_COMPILER_GCC
+#include "../../doctest.h"
+#if PL_COMPILER == PL_COMPILER_GCC
+#   pragma GCC diagnostic pop
+#endif // CR_COMPILER == CR_COMPILER_GCC
+#include "../../include/static_assert.hpp" // PL_TEST_STATIC_ASSERT
+#include "../../../include/pl/cont/at.hpp" // pl::cont::at
+#include <initializer_list> // std::intializer_list
+#include <string> // std::string
+#include <vector> // std::vector
+#include <type_traits> // std::is_same
+
+TEST_CASE("at_test")
+{
+    int a1[]{ 1, 2, 3 };
+    const int a2[]{ 4, 5, 6 };
+
+    std::vector<int> vec1{ 1, 2, 3, 4 };
+    const std::vector<int> vec2{ 5, 6, 7, 8 };
+    const std::vector<int> emptyVector{ };
+
+    std::initializer_list<std::string> il1{ "test", "text" };
+    const std::initializer_list<std::string> il2{ "Hello", "World" };
+    std::initializer_list<std::string> emptyInitList{ };
+
+    SUBCASE("type_test") {
+        PL_TEST_STATIC_ASSERT(
+            std::is_same<decltype(pl::cont::at(a1, 0U)), int &>::value);
+
+        PL_TEST_STATIC_ASSERT(
+            std::is_same<decltype(pl::cont::at(a2, 0U)), const int &>::value);
+
+        PL_TEST_STATIC_ASSERT(
+            std::is_same<decltype(pl::cont::at(vec1, 0U)), int &>::value);
+
+        PL_TEST_STATIC_ASSERT(
+            std::is_same<decltype(pl::cont::at(vec2, 0U)), const int &>::value);
+
+        PL_TEST_STATIC_ASSERT(
+            std::is_same<decltype(pl::cont::at(il1, 0U)), std::string>::value);
+
+        PL_TEST_STATIC_ASSERT(
+            std::is_same<decltype(pl::cont::at(il2, 0U)), std::string>::value);
+
+        CHECK_UNARY(true);
+    }
+
+    SUBCASE("positive_tests") {
+        CHECK(pl::cont::at(a1, 0U) == 1);
+        CHECK(pl::cont::at(a1, 1U) == 2);
+        CHECK(pl::cont::at(a1, 2U) == 3);
+
+        CHECK(pl::cont::at(a2, 0U) == 4);
+        CHECK(pl::cont::at(a2, 1U) == 5);
+        CHECK(pl::cont::at(a2, 2U) == 6);
+
+        CHECK(pl::cont::at(vec1, 0U) == 1);
+        CHECK(pl::cont::at(vec1, 1U) == 2);
+        CHECK(pl::cont::at(vec1, 2U) == 3);
+        CHECK(pl::cont::at(vec1, 3U) == 4);
+
+        CHECK(pl::cont::at(vec2, 0U) == 5);
+        CHECK(pl::cont::at(vec2, 1U) == 6);
+        CHECK(pl::cont::at(vec2, 2U) == 7);
+        CHECK(pl::cont::at(vec2, 3U) == 8);
+
+        CHECK(pl::cont::at(il1, 0U) == "test");
+        CHECK(pl::cont::at(il1, 1U) == "text");
+
+        CHECK(pl::cont::at(il2, 0U) == "Hello");
+        CHECK(pl::cont::at(il2, 1U) == "World");
+    }
+
+    SUBCASE("negative_tests") {
+        CHECK_THROWS_AS(pl::cont::at(a1, 3U), std::out_of_range);
+        CHECK_THROWS_AS(pl::cont::at(a2, 3U), std::out_of_range);
+
+        CHECK_THROWS_AS(pl::cont::at(vec1, 4U), std::out_of_range);
+        CHECK_THROWS_AS(pl::cont::at(vec2, 4U), std::out_of_range);
+        CHECK_THROWS_AS(pl::cont::at(emptyVector, 0U), std::out_of_range);
+
+        CHECK_THROWS_AS(pl::cont::at(il1, 2U), std::out_of_range);
+        CHECK_THROWS_AS(pl::cont::at(il2, 2U), std::out_of_range);
+        CHECK_THROWS_AS(pl::cont::at(emptyInitList, 0U), std::out_of_range);
+
+        CHECK_THROWS_AS(
+            pl::cont::at(a1, static_cast<std::size_t>(-1)),
+            std::out_of_range);
+        CHECK_THROWS_AS(
+            pl::cont::at(a2, static_cast<std::size_t>(-1)),
+            std::out_of_range);
+        CHECK_THROWS_AS(
+            pl::cont::at(vec1, static_cast<std::size_t>(-1)),
+            std::out_of_range);
+        CHECK_THROWS_AS(
+            pl::cont::at(vec2, static_cast<std::size_t>(-1)),
+            std::out_of_range);
+        CHECK_THROWS_AS(
+            pl::cont::at(emptyVector, static_cast<std::size_t>(-1)),
+            std::out_of_range);
+        CHECK_THROWS_AS(
+            pl::cont::at(il1, static_cast<std::size_t>(-1)),
+            std::out_of_range);
+        CHECK_THROWS_AS(
+            pl::cont::at(il2, static_cast<std::size_t>(-1)),
+            std::out_of_range);
+        CHECK_THROWS_AS(
+            pl::cont::at(emptyInitList, static_cast<std::size_t>(-1)),
+            std::out_of_range);
+    }
+}
