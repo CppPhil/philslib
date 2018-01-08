81478d3c313bd03eeea04c9c84eb65a503dda261
Phil <CppPhil@users.noreply.github.com>
2018-01-08 20:57:32 +0100

    alloca test

---
+++ test/src/alloca_test.cpp
@@ -0,0 +1,67 @@
+/* This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
+#include "../../include/pl/compiler.hpp"
+#if PL_COMPILER == PL_COMPILER_GCC
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wmissing-noreturn"
+#endif // CR_COMPILER == CR_COMPILER_GCC
+#include "../doctest.h"
+#if PL_COMPILER == PL_COMPILER_GCC
+#   pragma GCC diagnostic pop
+#endif // CR_COMPILER == CR_COMPILER_GCC
+#include "../../include/pl/alloca.hpp" // PL_ALLOCA
+#include "../../include/pl/byte.hpp" // pl::Byte
+#include <cstddef> // std::size_t
+#include <iterator> // std::distance
+#include <algorithm> // std::fill, std::all_of
+
+#if PL_COMPILER == PL_COMPILER_GCC
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wstack-protector"
+#endif // CR_COMPILER == CR_COMPILER_GCC
+TEST_CASE("alloca_test")
+{
+    static constexpr std::size_t byteCount{ 20U };
+    static constexpr pl::Byte    fillByte{ 0xAB };
+
+    void *memory = PL_ALLOCA(byteCount);
+
+    REQUIRE(memory != nullptr);
+
+    auto *begin = static_cast<pl::Byte *>(memory);
+    auto *end   = begin + byteCount;
+
+    REQUIRE(std::distance(begin, end) == byteCount);
+
+    std::fill(begin, end, fillByte);
+
+    CHECK_UNARY(std::all_of(
+        begin, end, [](pl::Byte byte) { return byte == fillByte; }));
+}
+#if PL_COMPILER == PL_COMPILER_GCC
+#   pragma GCC diagnostic pop
+#endif // CR_COMPILER == CR_COMPILER_GCC
