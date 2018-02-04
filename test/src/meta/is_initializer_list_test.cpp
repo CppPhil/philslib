3052655ed296e192f60f095858f31f01d00298fb
Phil <CppPhil@users.noreply.github.com>
2018-02-04 15:38:58 +0100

    is initlist tests

---
+++ test/src/meta/is_initializer_list_test.cpp
@@ -0,0 +1,81 @@
+/* This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
+#include "../../../include/pl/compiler.hpp"
+#if PL_COMPILER == PL_COMPILER_GCC
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wmissing-noreturn"
+#endif // PL_COMPILER == PL_COMPILER_GCC
+#include "../../doctest.h"
+#if PL_COMPILER == PL_COMPILER_GCC
+#   pragma GCC diagnostic pop
+#endif // PL_COMPILER == PL_COMPILER_GCC
+#include "../../include/static_assert.hpp" // PL_TEST_STATIC_ASSERT
+#include "../../../include/pl/meta/is_initializer_list.hpp" // pl::meta::is_initializer_list
+#include <ciso646> // not
+#include <initializer_list> // std::initializer_list
+#include <string> // std::string
+#include <vector> // std::vector
+
+namespace pl
+{
+namespace test
+{
+namespace
+{
+using Ty1 = std::initializer_list<int>;
+using Ty2 = std::initializer_list<double>;
+using Ty3 = std::initializer_list<std::string>;
+using Ty4 = std::string;
+using Ty5 = int;
+using Ty6 = std::vector<std::string>;
+} // anonymous namespace
+} // namespace test
+} // namespace pl
+
+TEST_CASE("is_initializer_list_positive_tests")
+{
+    PL_TEST_STATIC_ASSERT(
+        pl::meta::is_initializer_list<pl::test::Ty1>::value);
+    PL_TEST_STATIC_ASSERT(
+        pl::meta::is_initializer_list<pl::test::Ty2>::value);
+    PL_TEST_STATIC_ASSERT(
+        pl::meta::is_initializer_list<pl::test::Ty3>::value);
+
+    CHECK_UNARY(true);
+}
+
+TEST_CASE("is_initializer_list_negative_tests")
+{
+    PL_TEST_STATIC_ASSERT(
+        not pl::meta::is_initializer_list<pl::test::Ty4>::value);
+    PL_TEST_STATIC_ASSERT(
+        not pl::meta::is_initializer_list<pl::test::Ty5>::value);
+    PL_TEST_STATIC_ASSERT(
+        not pl::meta::is_initializer_list<pl::test::Ty6>::value);
+
+    CHECK_UNARY(true);
+}
